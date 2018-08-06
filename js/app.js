400087e3c45540a8864b5d9de6fe9924b89edfad
Hauke Bartsch <HaukeBartsch@gmail.com>
2018-08-06 15:15:19 -0700

    remember the first time you open the application

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+var disableLocalStorage = false;
 
 // logout the current user
 function logout() {
@@ -11,7 +12,46 @@ function logout() {
     });
 }
 
+// can we store data locally?
+function storageAvailable(type) {
+    if (disableLocalStorage)
+        return false;
+
+    try {
+        var storage = window[type],
+            x = '__storage_test__';
+        storage.setItem(x, x);
+        storage.removeItem(x);
+        return true;
+    } catch(e) {
+        return e instanceof DOMException && (
+            // everything except Firefox
+            e.code === 22 ||
+            // Firefox
+            e.code === 1014 ||
+            // test name field too, because code might not be present
+            // everything except Firefox
+            e.name === 'QuotaExceededError' ||
+            // Firefox
+            e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+            // acknowledge QuotaExceededError only if there's something already stored
+            storage.length !== 0;
+    }
+}
+
 jQuery(document).ready(function() {
-    jQuery('.starter').bind('animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd', function(e) { $(this).remove(); });
+    if (storageAvailable('localStorage')) {
+        var firstTime = localStorage.getItem('deap-first-time-load');
+	if (firstTime == "1") {
+	    jQuery('.starter').hide();
+	}
+    }
+	
+    jQuery('.starter').bind('animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd', function(e) {
+	$(this).remove();
+	if (storageAvailable('localStorage')) {
+            localStorage.setItem('deap-first-time-load', "1");
+	}
+    });
     jQuery('span.login__name').text(user_name);
 });
