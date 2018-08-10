519d15029a244c20283e797bab122cdf9e831da6
Hauke Bartsch <HaukeBartsch@gmail.com>
2018-08-10 08:40:17 -0700

    add geolocation as search term, add getting started page

---
+++ applications/Help/index.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Getting Started</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Le styles -->
+    <link href="../../css/bootstrap.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link href="../NewDataExpo/css/fontawesome-all.min.css" rel="stylesheet">
+
+    <!-- Latest compiled and minified CSS -->
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+        <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+
+    <!-- Fav and touch icons -->
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/img/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/img/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/img/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="/img/apple-touch-icon-57-precomposed.png">
+    <link rel="shortcut icon" href="/img/favicon.png">
+    <!--             <link href="https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css" rel="stylesheet"> -->
+    <!-- <link href="css/jquery-ui.min.css" rel="stylesheet"> -->
+    <link rel="stylesheet" href="css/style.css">
+
+  </head>
+
+  <body spellcheck="false">
+
+    <nav class="navbar navbar-expand-lg  navbar-light bg-light">
+      <a class="navbar-brand" href="#">DEAP Help</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="/index.php">Home <span class="sr-only">(current)</span></a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+
+    <div class="container-fluid" style="margin-top: 10px;">
+      <div class="row" style="padding-left: 10px; padding-right: 10px;">
+
+        <div class = "card">
+          <div class="card-header">
+            Getting Started
+          </div>
+          <div class="card-body">
+            <p>Start to get familiar with the ABCD way of naming variables by using the Explore application. Use the hierarchical view to get an overview by domains and the search field for a free-text search. You will notice that the variable names usually start with the name of the instrument used to capture the values. Similar values can be found therefore by changing the end of the search term.</p>
+            <p>Next use the Analysis application and just press the "Submit" button. Click on "Normal" in the menu and read up on the generated graphs. Try to add your own variables as dependend or independent variables.</p>
+            <p>Once you are familiar with this general workflow read up on the <i>Limit</i> and <i>Extend</i> applications to tailor your analysis.</p>
+          </div>
+        </div>
+        
+        <div class = "card">
+          <div class="card-header">
+            About
+          </div>
+          <div class="card-body">
+            <p>The Data Exploration and Analysis Portal (DEAP) provides advanced statistical analysis functions that are build to work with the Adolescent Brain Cognitive Development (ABCD) study - a long-term study of cognitive and brain development in children across the United States. From 2016-2018, children between the ages of 9-11 have been invited to join ABCD from 21 sites around the nation, with the intent to enroll and follow approximately 11,500 healthy children longitudinally for 10 years into young adulthood. </p>
+          </div>
+        </div>
+        
+        <div class = "card">
+          <div class="card-header">
+            Data
+          </div>
+          <div class="card-body">
+            <p>The data available on DEAP is a copy of the published data of the ABCD study on the National Data Archive (NDA). The data is pre-processed and merged into a single large database that is easiest understood as a spreadsheet. This process is documented on a public repository at <a href="https://github.com/ABCD-STUDY/analysis-nda17">github.com/ABCD-STUDY/analysis-nda17</a>. The current version of DEAP is based on the <i>ABCD Release 1.1</i> package under the doi: 10.15154/1460410.</p>
+            <p>Access to DEAP is only available to users with an existing NDA data use certification (<a href="https://data-archive.nimh.nih.gov/abcd">apply here</a>).</p>
+          </div>
+        </div>
+        
+        <div class = "card">
+          <div class="card-header">
+            How to cite DEAP
+          </div>
+          <div class="card-body">
+            <p>DEAP is a software provided by the Data Analysis and Informatics Center of ABCD located at UC San Diego under generous support by the National Institutes of Health and the Centers for Disease Control and Prevention under award number U24DA041123.</p>
+            <p>The DEAP project and its source code are available at <a href="https://scicrunch.org/resolver/SCR_016158">RRID: SCR_016158</a>.</p>
+            <p>The content is solely the responsibility of the authors and does not necessarily represent the official views of the National Data Archive or the National Institutes of Health.</p>
+          </div>
+        </div>
+        
+        <div class = "card">
+          <div class="card-header">
+            Overview
+          </div>
+          <div class="card-body">
+            <p>DEAP provides the means to find measures of interest from more than 40,000 shared observations available for each ABCD participant (see Explore). The user can select a sub-set of the participants for an hypothesis test using a multi-level regression model suitable for the ABCD study design (see Limit). Users are able to extend DEAP by their own measures (see Extend) and run novel multi-level statistical analysis directly from the DEAP interface (see Analyse).</p>
+          </div>
+        </div>
+
+        <div class = "card">
+          <div class="card-header">
+            Analyze
+          </div>
+          <div class="card-body">
+            <p>The statistical analysis package used by DEAP is based on the R-package <a href="https://cran.r-project.org/web/packages/gamm4/"><i>gamm4</i></a> and implements a generalized additive mixel model that captures the degrees of freedom in a longitudinal multi-site study that includes a complex family structure. The user interface of the analysis application provides meaningful presets for covariates of no interest and in general the user is only required to change the variables of interest, the dependend the and independed variable to start the analysis.</p>
+            <p>Learn how to load and save a model (see the menu button on the top left).</p>
+            <p>More extensive help is available in the application itself. The menu option <i>Normal</i> can be switched to <i>Tutorial</i> mode which shows text on how to interpret the generated graphs and tables.</p>
+          </div>
+        </div>
+
+        <div class = "card">
+          <div class="card-header">
+            Limit
+          </div>
+          <div class="card-body">
+            <p>The statistical analysis in the Analyze application by default applies to all participants with data. In order to limit the analysis to a subset of participants the <i>Limit</i> application is used to define the list of participants. These named lists are saved and can be selected in the Analysis application.</p>
+            <p>Help on the syntax of the subsetting is available in the Limit application.</p>
+          </div>
+        </div>
+
+        <div class = "card">
+          <div class="card-header">
+            Extend
+          </div>
+          <div class="card-body">
+            <p>If you want to add measures to DEAP you can do this using the <i>Extend</i> application. It is not able to add your participants, but you can change and combine existing measures and use them during the analysis.</p>
+          </div>
+        </div>
+
+        <div class = "card">
+          <div class="card-header">
+            Hypothesis Registration
+          </div>
+          <div class="card-body">
+            <p>We support an hypothesis driven workflow on DEAP by providing material for registering your hypothesis using a sampling plan, design plan, analysis plan and analysis scripts. Please see the information in the <i>Plan</i> application.</p>
+          </div>
+        </div>
+
+        
+
+      </div>
+      <div style="margin-bottom: 60px;">&nbsp;</div>
+    </div>
+  </body>
+
+  <script src="../../js/jquery-3.3.1.min.js"></script>
+  <script src="../../js/bootstrap.min.js"></script>
+  <script src="../../js/d3.v3.min.js"></script>
+
+</html>
