2e506d717b7e4b286aa251fe964211e318e12ec4
Hauke Bartsch <HaukeBartsch@gmail.com>
2018-08-10 10:24:57 -0700

    try to make docker single shared directory work

--- code/setup.sh
+++ code/setup.sh
@@ -25,9 +25,11 @@ if [ -z "${dataRds}" ] && [ ! -f "${dataRds}" ]; then
     echo "Detected initial data Rds in project data directory, start setup..."
     mkdir -p "${rootpath}/data/${project}/data_uncorrected"
     mkdir -p "${rootpath}/data/${project}/Ontology/searchServer/"
-    cp "${rootpath}/applications/Ontology/searchServer/teach.json" "${rootpath}/data/${project}/Ontology/searchServer/"
+    cp "${rootpath}/applications/Ontology/searchServer/teach.json" "${rootpath}/data/${project}/Ontology/"
     mkdir -p "${rootpath}/data/${project}/NewDataExpo/usercache/"
     mkdir -p "${rootpath}/data/${project}/Pre-Registration/"
+    mkdir -p "${rootpath}/data/${project}/ModelBuilder/viewer/recipes/"
+    cp "${rootpath}/applications/ModelBuilder/viewer/recipes/GAMM4-FZ-CR.*" "${rootpath}/data/${project}/ModelBuilder/viewer/recipes/"
 
     # the owner of all of these should be the web-user (fixed user across all docker instances)
     chown -R www-data:www-data "${rootpath}/data/${project}/"
