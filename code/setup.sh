c51c1759f1f06f69cee3d71651c9a4f990a2fb5b
Hauke Bartsch <HaukeBartsch@gmail.com>
2018-10-20 13:39:19 -0700

    wildcard needs to be outside of double quotes

--- code/setup.sh
+++ code/setup.sh
@@ -43,7 +43,7 @@ if [ -f "${dataRds}" ] && [ ! -f "${dataRdsEnd}" ]; then
 
     mkdir -p "${rootpath}/data/${project}/Filter/data/"
     cp "${rootpath}/applications/Filter/data/public.json" "${rootpath}/data/${project}/Filter/data/public.json"
-    cp "${rootpath}/applications/Filter/data/filterSets*.json" "${rootpath}/data/${project}/Filter/data/"
+    cp "${rootpath}/applications/Filter/data/filterSets"*.json "${rootpath}/data/${project}/Filter/data/"
     mkdir -p "${rootpath}/logs"
     chown -R www-data:www-data "${rootpath}/logs"
     
