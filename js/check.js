28b2bac9e1fd45397557e4498f2c03b00a61924f
Fangzhou Hu <fhu6@wisc.edu>
2019-04-08 17:11:54 +0000

    fix income level formatting

--- js/check.js
+++ js/check.js
@@ -3,7 +3,7 @@ $( document ).ready(function() {
 	data = JSON.parse(data);
        if(Array.isArray( data["Rserve-status"])){
              if(data["Rserve-status"][0] > 100 &&  data["Rserve-status"][1] > 3){
-                jQuery("body").append("<span class = 'dot'>&nbsp;R</span>")
+                jQuery("body").append("<span class = 'dot'>R</span>")
              }
 	     if( JSON.parse(localStorage.getItem('src_subject_id')) && JSON.parse(localStorage.getItem('src_subject_id')).length != data["Rserve-status"][0]){
 		window.localStorage.clear();
