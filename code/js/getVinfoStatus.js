aa5a579cfcba86b51b0d0fcecd2b1a34f77cfbeb
Fangzhou Hu <fhu6@wisc.edu>
2019-04-12 23:30:52 +0000

    fix code/js/getVinfoStatus.js

--- code/js/getVinfoStatus.js
+++ code/js/getVinfoStatus.js
@@ -7,12 +7,10 @@ if (process.argv.length <= 2) {
 
 var cols = process.argv[2];
 
-fs.readdir("/var/www/html/data/ABCD/NewDataExpo/variableInfo", function(err, items) {
-
-    //console.log(items.length <= cols);
-    if (typeof items !== 'undefined' && items.length <= cols){
-	return true;
-    } else {
-	return false;
+fs.readdir("/var/www/html/data/ABCD/NewDataExpo/variableInfo/", function(err, items) {
+    console.log(items.length)
+    if(items){
+	return items.length;
     }
+    return "!";
 });
