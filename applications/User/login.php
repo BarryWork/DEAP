7b8a6d3e12fac171646522bdc9d27591cd5c5212
Fangzhou Hu <fhu6@wisc.edu>
2019-05-28 20:17:53 +0000

    remove changes to login and AC

--- applications/User/login.php
+++ applications/User/login.php
@@ -7,31 +7,26 @@ include("../../code/php/AC.php");
 $incorrect = '';
 
 if (isset($_POST["ac"]) && $_POST["ac"]=="log") { /// do after login form is submitted
-    // ok, now we have the real password here for the username, test that on NDA
-    $pw = $_POST['pw'];
-    $user = $_POST['username'];
     
-    $url = 'https://deapauth.nimhda.org/user/permissiongroup/Adolescent%20Brain%20Cognitive%20Development';
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL,$url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
-    curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-    curl_setopt($ch, CURLOPT_USERPWD, "$user:$pw");
-    $result = curl_exec($ch);
-    curl_close($ch);
-    $res = json_decode($result,true);
-
-    // {"permissionGroupTitle":"Adolescent Brain Cognitive Development","hasPermission":true,"expirationDate":"2018-12-02","status":"Approved"}
-    if (isset($res['hasPermission']) && $res['hasPermission'] == "true" && $res['status'] == "Approved") {
-        // ok, assume we have the permissions
-        $_SESSION["logged"]=$user;        
+    ////////////////////////
+    // BLOCK ALL BUT ADMIN
+    ////////////////////////
+    if ($USERS[$_POST["username"]]==$_POST["pw"]) { /// check if submitted username and password exist in $USERS array
+        // as a features users can login using their email address
+        // here we need to get the real user name and use that instead of the email address
+        if (strpos($_POST["username"], "@") !== false) {
+            // found email as user name, what is the real name?
+            $_SESSION["logged"]=getUserNameFromEmail($_POST["username"]);
+        } else {
+            $_SESSION["logged"]=$_POST["username"];
+        }
+        
     } else {
         audit( "login", "incorrect password for ".$_POST["username"] );
-        $incorrect = 'Incorrect username/password. Please, try again. If this error persists please make sure that you have access to the ABCD project data on the National Data Archive (NDA helpdesk at ndahelp@mail.nih.gov). Make sure your account on NDA is current, permissions only last a year before a renewal is required. NDA error message: '.$res['error'].', '.$res['message'].'.';
-    }
-    // echo($result);
+        $incorrect = 'Incorrect username/password. Please, try again.';
+    };
 };
-if (isset($_SESSION["logged"])) {
+if (isset($_SESSION["logged"]) && array_key_exists($_SESSION["logged"],$USERS)) {
     $l = strlen('/login.php');
     if (isset($_POST["url"]) && $l > 0 && substr($_POST["url"],-$l) === '/login.php') {
         $u = $_POST["url"];
@@ -58,7 +53,7 @@ if (isset($_SESSION["logged"])) {
 	<link href="css/bootstrap-responsive.min.css" rel="stylesheet">
 	<link href="css/font-awesome.min.css" rel="stylesheet">
 	<link href="css/bootswatch.css" rel="stylesheet">
-        <link href="//ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/start/jquery-ui.css" rel="stylesheet" type="text/css"/>
+        <link href="css/jquery-ui.theme.min.css" rel="stylesheet" type="text/css"/>
         <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
         <!--[if lt IE 9]>
           <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
@@ -75,18 +70,12 @@ if (isset($_SESSION["logged"])) {
   <div class="container">
    <div class="row">
      <div class="hero-unit">
-       <h1>Data Analysis and Exploration Portal (DEAP)</h2>
-       <center><i>DEAP software was developed by the <a href="https://abcd-workspace.ucsd.edu">Data Analysis and Informatics Core</a> of the <a href="http://abcdstudy.org">ABCD Study</a></i></center>
-
+       <h1>Data Analysis and Exploration Portal</h2>
+       <center><i>A service provided by the <a href="https://abcd-workspace.ucsd.edu">Data Analysis and Informatics Core</a> of <a href="http://abcdstudy.org">ABCD</a></i></center>
+       <p class="lead">
+         This page requires a login. Logins are provided by your system administration.
+       </p>
      </div>
-       <div class = span-1></div>
-       <div class = "span-10"><p class="lead">
-DEAP provides advanced statistical analysis functions to work with the most recent 2.0 data release from the Adolescent Brain Cognitive Development (ABCD) Study - a long-term study of cognitive and brain development in children across the United States.
-DEAP access is available to users with current access to the ABCD Study.  ABCD Study data are housed at the <a href="https://data-archive.nimh.nih.gov/abcd">NIMH Data Archive</a>. Use your NDA credentials to log in and being exploring the data.
-
-       </p></div>
-       <div class = span-1></div>
-
    </div>
    <div class="row">
     <div class="span4"> </div>
@@ -94,7 +83,7 @@ DEAP access is available to users with current access to the ABCD Study.  ABCD S
       <form action="login.php" method="post" id="login-form">
          <input type="hidden" name="ac" value="log">
          <input type="hidden" name="pw" id="pw">
-         <input type="hidden" name="url" id="url" value="<?php if (isset($_GET['url'])) { echo $_GET['url']; } else { echo("/index.php"); } ?>">
+         <input type="hidden" name="url" id="url" value="<?php echo $_GET['url']; ?>">
          <input type="text" name="username" placeholder="user" class="span3" autofocus/>
       </form>
       <input id="pw-field" type="password" name="password" placeholder="********" onkeypress="handleKeyPress(event, this.form)" class="span3"><br/>
@@ -104,30 +93,10 @@ DEAP access is available to users with current access to the ABCD Study.  ABCD S
       </div>
     </div>
    </div>
-   <div class="row">
-    <br>
-    <div class="span2"></div>
-     <div class="span7"><center><i>
-        If you do not have access, <a href="https://nda.nih.gov/abcd/request-access">request an NDA account</a> to begin the data access request process. 
-     </center></i></div>
-         </div>
-
-
-   </div> <!--container-->
-  <div class= "container" style="padding-top:15%">
-    <div class = "row" >
-      <div class = "span-1" style = "position:relative" ></div>
-       <div class = "span-8" style = "position:relative">
-        <p>
-          DEAP and ABCD data are housed in the NIMH Data Archive, a US government cloud computing system.  All users are expected to adhere to responsible data use behavior as outlined in the NIMH Data Archive <a href="https://ndar.nih.gov/ndarpublicweb/Documents/NDAR+Data+Access+Request+DUC+FINAL.pdf">Data Use Certification agreement</a>.  Violation of these terms and conditions could result in revoked access or other actions.
-        </p>
-       </div>
-      <div class = "span-1"></div>
-    </div>
-  </div> 
-
-  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-  <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.min.js"></script>
+  </div>
+  
+  <script src="../../js/jquery.min.js"></script>
+  <script src="../../js/jquery-ui.min.js"></script>
   <!-- create an md5sum version of the password before sending it -->
   <script src="../../js/md5-min.js"></script>
 
@@ -138,7 +107,7 @@ DEAP access is available to users with current access to the ABCD Study.  ABCD S
             if (incorrect.length > 0) {
                 alert(incorrect);
             } else {
-                alert('Wrong user name or wrong password, please try again. If this error persists please make sure that you have access to the ABCD project data on the National Data Archive (NDA helpdesk at ndahelp@mail.nih.gov).');
+                alert('Wrong user name or wrong password, please try again.');
             }
         }
 
@@ -155,16 +124,13 @@ DEAP access is available to users with current access to the ABCD Study.  ABCD S
      
         // calculate and copy hash value after entering password
         jQuery('#pw-field').blur(function() {
-            rewritePW();
+	   rewritePW();
         });
      });
 
      function rewritePW() {
            hash = hex_md5(jQuery('#pw-field').val());
-           // we don't want to create the hash here, we need the full password - sounds dangerous
-           // but we are talking to our own system here and we are using https.
-           jQuery('#pw').val(jQuery('#pw-field').val());
-           //jQuery('#pw').val(hash);     
+           jQuery('#pw').val(hash);     
      }
 
      function handleKeyPress(e, form) {
