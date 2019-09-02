f802702a2b8fb464d032abfa29715b648fd690ae
Hauke Bartsch <HaukeBartsch@gmail.com>
2019-09-02 09:58:59 +0200

    use boostrap event instead

--- applications/medications/js/all.js
+++ applications/medications/js/all.js
@@ -663,7 +663,7 @@ jQuery(document).ready(function() {
         
     });
 
-    jQuery('#create-new-score').on('show', function() {
+    jQuery('#create-new-score').on('Show.bs.modal', function() {
 	console.log("show the dialog");
     });
     
