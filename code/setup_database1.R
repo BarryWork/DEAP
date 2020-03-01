ce3e9017cc09dc70ae632466686efaf889f37763
Hauke Bartsch <HaukeBartsch@gmail.com>
2020-03-01 15:15:57 +0100

    use COPY INTO to seed up import, document expected behavior

--- code/setup_database1.R
+++ code/setup_database1.R
@@ -1,52 +1,86 @@
-#library(DBI)
-#options(monetdb.debug.query=TRUE)
-#nda18 = readRDS('/var/www/html/data/ABCD/data_uncorrected/nda18.Rds')
-## sanitize the column names by removing list of characters
-#names(nda18) = gsub("([_.-])", "", names(nda18))
-## convert all factors to numbers
-#for (i in seq(1, ncol(nda18))) {
-#    if (class(nda18[[i]]) == "factor") {
-#        nda18[[i]] = as.numeric(levels(nda18[[i]]))[nda18[[i]]]
-#    }
-#}
-#chunk <- 500
-#n <- nrow(nda18)
-#r <- rep(1:ceiling(n/chunk),each=chunk)[1:n]
-#d <- split(nda18,r)
-#conn <- dbConnect(MonetDBLite::MonetDB.R(), host="localhost", port=11223, dbname="abcd", user="monetdb", password="monetdb", timeout=15)
-#dbSendQuery(conn, "DROP TABLE IF EXISTS abcd")
-#for (i in seq(1, length(d))) {
-#   dbWriteTable(conn, "abcd", d[[i]], append=TRUE, csvdump=TRUE, row.names = FALSE)
-#}
+#
+# Convert an R data.frame to a MonetDB compatible import.
+#
+# This functionality mimics an sqldump suitable for fast import into MonetDB (database abcd).
+# To import the resulting files into MonetDB do (in bash):
+# > DOTMONETDBFILE=/root/.monetdb_root mclient --port=11223 -d abcd /tmp/createTable.sql
+#
+# Speed: The "COPY RECORDS" method below is the fastest way to import large data into MonetDB.
+#        That said, the speed of the import of the table is still slow (call to mclient). 
+#
+# There will be several tables based on a calculated prefix derived from each item name.
+# Attention: The prefix is not derived from actual NDA instruments. We just split by "_.-"
+#            and use the first entries for each. Also, all splits that produce groups with 
+#            less than 40 items will be combined into a general "abcd_other" table.
+#
+# The resulting MonetDB database can be used to extract values from it by:
+#  - check if the column you are looking for is in the table "abcd_other"
+#  - if not, split the item name using [_.-] and create a table name using the index 0 string
+#    prefixed with "abcd_". For example "fam_roster_2c_v2_l" will be in table "abcd_fam".
+#
+# All tables in the MonetDB database will have the two columns "subjectid" and "eventname" as
+# VARCHAR. All other columns are numeric where numeric values appear as REAL data type and factors
+# are coded as INT based on their levels in R. As an example the "fam_roster_2c_v2_l" in R
+# has levels:
+# > levels(nda18['fam_roster_2c_v2_l'])
+#  [1] "Husband or wife"            "Unmarried partner"         
+#  [3] "Father or mother"           "Foster child"              
+#  [5] "Grandchild"                 "Other nonrelative"         
+#  [7] "Parent-in-law"              "Biological son or daughter"
+#  [9] "Other relative"             "Adopted son or daughter"   
+# [11] "Roomer or boarder"          "Housemate or roommate"     
+# [13] "Brother or sister"         
+#
+# In MonetDB this column would have integers 1:13 with 3 meaning "Father or mother" etc..
+#
 
-# for test purposes lets limit the columns of our table to data without these
-dontuse=c("^tfmri", "^smri", "^dmri", "^rsfmri", "^iqc")
-nda18 = readRDS('/var/www/html/data/ABCD/data_uncorrected/nda18.Rds')
+# for test purposes lets limit the columns of our table to data without these instruments
+dontuse=c("^tfmri", "^rsfmri")
+nda18 = readRDS('/var/www/html/data/ABCD/data_uncorrected/nda18.Rds_copy')
 for (u in dontuse) { 
     nda18 = nda18[,grep(u, names(nda18), invert=TRUE)]; 
 }
-# we have to save each table individually - if the table has more than 1000 entries 
-# the creation of the table will take too much time
 othercolumns=names(nda18) # without the musthaves
 othercolumns=othercolumns[othercolumns != "subjectid"]
 othercolumns=othercolumns[othercolumns != "eventname"]
 prefixes=gsub("([^_.-]+[_.-]).*", "\\1", othercolumns)
 musthaves=c("subjectid", "eventname")
-howmany = sort(table(prefixes)) # the values are sorted
+howmany = sort(table(prefixes)) # the values are sorted by how many items per 'instrument'
 cutoff=40 # any less entries and all go into the first table, all others get individual tables
-# names(nda18) = gsub("([_.-])", "", names(nda18))
-tab="abcd_other"
+tab="abcd_other" # first table that gets the small fry
 sqldump1 <- array()
 sqldump1[[length(sqldump1)]] = paste("DROP TABLE IF EXISTS", tab, ";", sep=" ")
 sqldump1[[length(sqldump1)+1]] = paste("CREATE TABLE", tab, "( \"subjectid\" VARCHAR(100), \"eventname\" VARCHAR(100)", sep=" ")
-# we can have partial fits - like in kbipcserviceschecklistl1 and kbipcserviceschecklistl10
-# for those we keep a list of havedones. If we don't do this we will add some columns twice - fails table creation!
+# we can have partial matches - like in kbipcserviceschecklistl1 and kbipcserviceschecklistl10
+# for those we keep a list of 'havedones'. If we don't do this we will add some columns twice - fails table creation!
 havedones = array()
+lasttablescolumnnames = array() # remember all columns in all previous tables to get columns in current table
 for (t in names(howmany)) {
   if (howmany[t] > cutoff) {
     sqldump1[[length(sqldump1)+1]] = " );"
     # make subjectid and eventname the unique keys in the table
     sqldump1[[length(sqldump1)+1]] = paste("ALTER TABLE", tab, "ADD CONSTRAINT ",paste(tab, "_uc", sep="")," UNIQUE (\"subjectid\", \"eventname\"); ", sep=" ")
+    # now we can create a CSV for this table and import (get names for all columns first - has to be in order)
+    tcolumns = c("subjectid", "eventname", havedones[!(havedones %in% lasttablescolumnnames)])
+    dt = nda18[,tcolumns]
+    fac=names(dt)[sapply(dt, class) == "factor"]
+    i = 3 # we always have two columns first that should stay VARCHAR (subjectid and eventname)
+    while(i <= length(fac)) {
+      # converting factors to numeric values will fail if we don't code levels and labels correctly, levels should be numeric
+      if (!is.numeric(levels(dt[fac[[i]]]))) {
+        print(paste("warning: non-numeric levels for", fac[[i]], "we will create our own levels."))
+        lab=levels(dt[[fac[[i]]]])
+        if (length(lab) > 0)
+          dt[fac[[i]]] = match(dt[[fac[[i]]]], lab) # factor(match(dt[[fac[[i]]]], lab), labels=lab, levels=1:length(lab))
+      }
+      dt[fac[[i]]] = as.numeric(dt[[fac[[i]]]])
+      i = i + 1;
+    }
+    tname = paste("/tmp/", tab, ".csv",sep="")
+    write.table(dt, file=tname, row.names=FALSE, col.names=FALSE, na="", sep="|")
+    sqldump1[[length(sqldump1)+1]] = paste("COPY", nrow(dt), "RECORDS INTO", tab, "FROM", paste("'", tname, "'", sep=""), "USING DELIMITERS '|','\\n','\"' NULL AS '';", sep=" ")
+    lasttablescolumnnames = havedones
+
     tab = paste("abcd",gsub("[_.-]", "", t),sep="_")
     sqldump1[[length(sqldump1)+1]] = paste("DROP TABLE IF EXISTS", tab, ";", sep=" ")
     sqldump1[[length(sqldump1)+1]] = paste("CREATE TABLE", tab, "( \"subjectid\" VARCHAR(100), \"eventname\" VARCHAR(100)", sep=" ")
@@ -74,75 +108,5 @@ con <- file(tmp, "w")
 writeLines(paste0(sqldump1, collapse=""), con=con)
 close(con)
 
-# mclient -u monetdb --port=11223 -d abcd /tmp/Rtmp3AbMKr/createTablec38e5dc9929a
+# Should be tested, running it on command line using 'pv' timing is not too bad (not days but hours).
 system(paste("DOTMONETDBFILE=/root/.monetdb_root mclient --port=11223 -d abcd ", tmp, sep=""))
-
-# Next create the data (again, takes a long time - hours)
-dontuse=c("^tfmri", "^smri", "^dmri", "^rsfmri", "^iqc")
-nda18 = readRDS('/var/www/html/data/ABCD/data_uncorrected/nda18.Rds')
-for (u in dontuse) { 
-    nda18 = nda18[,grep(u, names(nda18), invert=TRUE)]; 
-}
-# we have to save each table individually - if the table has more than 1000 entries 
-# the creation of the table will take too much time
-othercolumns=names(nda18) # without the musthaves
-othercolumns=othercolumns[othercolumns != "subjectid"]
-othercolumns=othercolumns[othercolumns != "eventname"]
-prefixes=gsub("([^_.-]+[_.-]).*", "\\1", othercolumns)
-musthaves=c("subjectid", "eventname")
-howmany = sort(table(prefixes)) # the values are sorted
-cutoff=40 # any less entries and all go into the first table, all others get individual tables
-#names(nda18) = gsub("([_.-])", "", names(nda18))
-tab="abcd_other"
-tmp2 <- '/tmp/fillData.sql'
-con2 <- file(tmp2, "w")
-tab="abcd_other"
-inserted = array() # we only want to insert the unique keys once, keep track of them here
-for (t in names(howmany)) {  # now go through all tables (sorted)
-  if (howmany[t] > cutoff) {
-      tab = paste("abcd",gsub("[_.-]", "", t),sep="_")
-      inserted = array()
-  }
-  idx = grep(paste("^",t,sep=""), names(nda18))
-  idx = c(c(1, 3), idx)
-  data2 = nda18[, idx, drop=FALSE] # prevent a vector, need data.table
-  # separate loops for the unique keys (INSERT INTO) and later the UPDATE
-  for (row in seq(1, nrow(data2))) {
-    r = data2[row, ,drop=FALSE]
-    idx = !is.na(r)
-    r = r[, idx]
-    if (length(r) <= 2) {
-      next
-    }
-    k = paste(as.character(r[[1]]), as.character(r[[2]]), sep="")
-    if ( k %in% inserted ) {
-      next
-    }
-    inserted = append(inserted, k);
-    writeLines(paste("INSERT INTO ", tab, " ( ", paste("\"", musthaves, "\"", sep="", collapse=", "), " ) VALUES ( ", paste("'", as.character(r[[1]]), "'", sep=""), ",", paste("'", as.character(r[[2]]), "'", sep="") , ");"), con=con2)
-  }
-  cnames=names(data2)
-  for (row in seq(1, nrow(data2))) {
-    # dbSendQuery(conn, paste("CREATE TABLE abcd ( ", toString(codes), " );"), csvdump=TRUE);
-    r = data2[row, ,drop=FALSE]
-    idx = !is.na(r)
-    r = r[, idx]
-    if (length(r) <= 2) {
-      next
-    }
-    fac=names(r)[sapply(r, class) == "factor"]
-    i = 3 # we always have two columns first that should stay VARCHAR (subjectid and eventname)
-    while(i <= length(fac)) {
-      r[fac[[i]]] = as.numeric(as.character(r[fac[[i]]]))
-      i = i + 1;
-    }
-    n = paste("\"", names(r[3:length(r)]), "\"", sep="")
-    v = r[,3:length(r)]
-    if (length(n) > 0 && length(n) == length(v)) {
-      writeLines(paste("UPDATE ", tab, " SET ", paste(n, v, sep=" = ", collapse=", "), " WHERE \"subjectid\" = '", as.character(r[[1]]), "' AND \"eventname\" = '", as.character(r[[2]]), "';", sep=""), con=con2)
-    }
-  }
-}
-close(con2)
-
-system(paste("DOTMONETDBFILE=/root/.monetdb_root mclient --port=11223 -d abcd ", tmp2, sep=""))
